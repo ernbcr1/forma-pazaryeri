e003432c2ff28b675e20d7b37903d9325f5ea04d
Eren Bicer <ernbcr1@gmail.com>
2026-06-24 10:53:10 +0300

    Update favicon and apple icon

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -1,37 +1,37 @@
 import { ImageResponse } from "next/og";
 
 export const size = {
-  width: 64,
-  height: 64,
+  width: 180,
+  height: 180,
 };
 
 export const contentType = "image/png";
 
-export default async function Icon() {
+export default async function AppleIcon() {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://elformazione.com";
 
   return new ImageResponse(
     (
       <div
         style={{
-          width: "64px",
-          height: "64px",
+          width: "180px",
+          height: "180px",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           background: "#ffffff",
-          borderRadius: "18px",
+          borderRadius: "42px",
           overflow: "hidden",
         }}
       >
         <img
           src={`${siteUrl}/elf-icon.jpg`}
           alt="elFormazione"
-          width="64"
-          height="64"
+          width="180"
+          height="180"
           style={{
-            width: "64px",
-            height: "64px",
+            width: "180px",
+            height: "180px",
             objectFit: "cover",
           }}
         />
